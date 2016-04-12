31312d01ef8bcc0b166b98402c79f4db60c0fd65
Justin.Leach <web.justin.leach@gmail.com>
2016-04-12 15:49:45 -0500

    databinding

--- src/js/components/Header.jsx
+++ src/js/components/Header.jsx
@@ -6,12 +6,16 @@ import React from 'react';
 import Title from './Header/Title.jsx';
 
 export default class Header extends React.Component {
-
+    handleChange(e) {
+        const title = e.target.value;
+        this.props.changeTitle(title);
+    }
 
     render() {
         return (
             <header>
                 <Title title={this.props.title}/>
+                <input value={this.props.title} onChange={this.handleChange.bind(this)} type="text"/>
             </header>
         )
     }
